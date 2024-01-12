e1552cadb9185199921555a751bc81be30488f30
Nguyen-huan <huan06949@gmail.com>
2024-01-12 11:17:48 +0700

    update wordpress version 6.4.2

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitbead3d4fadfefcd56b6039baf910499d::getLoader();
+return ComposerAutoloaderInit5f4ae8f81d6d3a5cc97a1356d62f1937::getLoader();
